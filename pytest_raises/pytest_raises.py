73fc9a9be926881acc7facecaa0e408dcdb2ddd7
JamesMakela <james.makela@gmail.com>
2018-10-29 22:42:01 -0700

    Some pep8 cleanup

--- pytest_raises/pytest_raises.py
+++ pytest_raises/pytest_raises.py
@@ -20,7 +20,10 @@ def pytest_runtest_call(item):
     # In order to maintain compatability, prefer the newer function
     # (get_closest_marker) but use the old function (get_marker) if it
     # doesn't exist.
-    marker_get_func = item.get_closest_marker if hasattr(item, 'get_closest_marker') else item.get_marker
+    if hasattr(item, 'get_closest_marker'):
+        marker_get_func = item.get_closest_marker
+    else:
+        marker_get_func = item.get_marker
 
     raises_marker = marker_get_func('raises')
     if raises_marker:
@@ -30,13 +33,15 @@ def pytest_runtest_call(item):
 
         raised_exception = outcome.excinfo[1] if outcome.excinfo else None
         traceback = outcome.excinfo[2] if outcome.excinfo else None
+
         if isinstance(raised_exception, exception):
             outcome.force_result(None)
             if message is not None:
                 try:
                     raised_message = str(raised_exception)
                     if message not in raised_message:
-                        raise ExpectedMessage('"{}" not in "{}"'.format(message, raised_message))
+                        raise ExpectedMessage('"{}" not in "{}"'
+                                              .format(message, raised_message))
                 except(ExpectedMessage):
                     excinfo = sys.exc_info()
                     if traceback:
@@ -45,7 +50,10 @@ def pytest_runtest_call(item):
                         outcome._excinfo = excinfo
         else:
             try:
-                raise raised_exception or ExpectedException('Expected exception {}, but it did not raise'.format(exception))
+                raise (raised_exception or
+                       ExpectedException('Expected exception {}, '
+                                         'but it did not raise'
+                                         .format(exception)))
             except(ExpectedException):
                 excinfo = sys.exc_info()
                 if traceback:
